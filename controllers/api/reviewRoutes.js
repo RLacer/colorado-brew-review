f8988db4b696c40508fcfd52e40a7fa42d51edaa
RLacer <r.a.lacer@hotmail.com>
2021-05-24 08:56:02 -0600

    adding db, seeds, models & login

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Review } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    //this is what's creating it in the db.  req.body is creating  it and associating it  with the user id.
+    const newReview = await Review.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+//check  id for delete
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const reviewData = await Review.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!reviewData) {
+      res.status(404).json({ message: 'No review found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ models/Review.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model { }
+
+Review.init(
+	{
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          brewery_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          city: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+          description: {
+            type: DataTypes.STRING,
+          },
+            user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+          },
+		
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'review'
+	}
+);
+
+module.exports = Review;
